8af8c0c75dfecc6fbaa6b23eea0047bdfb67b2db
Bartosz Chojnacki <bartosz.chojnacki4@gmail.com>
2022-10-16 06:29:07 +0200

    module 4

--- jobs/app.py
+++ jobs/app.py
@@ -22,12 +22,13 @@ def execute_sql(sql, values=(), commit=False, single=False):
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
+        cursor.close()
     return results
 
 
 @app.teardown_appcontext
 def close_connection(exception):
-    connection = getattr(g, '_connection', None)
+    connection = getattr(g, '._connection', None)
     if connection is not None:
         connection.close()
 
