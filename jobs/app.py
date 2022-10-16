894d9e5245ae3437fe9783877019ecc898d32b03
Bartosz Chojnacki <bartosz.chojnacki4@gmail.com>
2022-10-16 06:33:10 +0200

    module 4

--- jobs/app.py
+++ jobs/app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 
 
 def open_connection():
-    connection = getattr(g, '._connection', None)
+    connection = getattr(g, '_connection', None)
     if connection is None:
-        connection, g._connection = sqlite3.connect(PATH)
+        connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
 
@@ -29,7 +29,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
 
 @app.teardown_appcontext
 def close_connection(exception):
-    connection = getattr(g, '.connection', None)
+    connection = getattr(g, '_connection', None)
     if connection is not None:
         connection.close()
 
